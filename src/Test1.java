07cd2614b3a759fedd50c3c979c500dd4d85acdd
dhruvsujatha <dsujatha@purdue.edu>
2021-11-05 22:19:55 -0400

    Changed Switch to IfElse

--- src/Test1.java
+++ src/Test1.java
@@ -9,25 +9,40 @@ public class Test1 {
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
 
-        switch(operation) {
-            case ADD:
-                // if operation = ADD, return the sum
-                result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
-                result = num1 * num2;
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
+        // switch(operation) {
+        //     case ADD:
+        //         // if operation = ADD, return the sum
+        //         result = num1 + num2;
+        //         break;
+        //     case SUBTRACT:
+        //         result = num1 - num2;
+        //         break;
+        //     case MULTIPLY:
+        //         result = num1 * num2;
+        //         break;
+        //     case DIVIDE:
+        //         if (num2 == 0) {
+        //             System.out.println("Can't divide by 0");
+        //         } else {
+        //             result = ((double)num1) / num2;
+        //         }
+        //         break;
+        // }
+
+        if (operation == ADD) {
+            result = num1 + num2;
+        } else if (operation == SUBTRACT) {
+            result = num1 - num2;
+        } else if (operation == MULTIPLY) {
+            result = num1 * num2;
+        } else {
+            if (num2 == 0) {
                     System.out.println("Can't divide by 0");
                 } else {
                     result = ((double)num1) / num2;
                 }
-                break;
         }
+
         return result;
     }
 
